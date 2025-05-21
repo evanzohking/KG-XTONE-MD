a4daaed9d385631b38db14e58ac9b093ba7f66a1
evanzohking <evansetyang12@gmail.com>
2025-05-22 02:04:34 +0300

    settings.js

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: 'KG-XTONE-MD',
+  author: '‎',
+  botName: "KG-XTONE-MD",
+  botOwner: 'KGEvans', // Your name
+  ownerNumber: '254791002497', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  description: "KG-XTONE MULTI DEVICE IS A SIMPLE GITHUB PROGRAMME WHICH YOU GIVE MORE HELP.",
+  version: "1.0.0",
+};
+
+module.exports = settings;
